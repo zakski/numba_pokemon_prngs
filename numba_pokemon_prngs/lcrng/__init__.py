a2495fd1286b065cd237e23e58939c34a4fa4aea
lincoln-lm <73306575+Lincoln-LM@users.noreply.github.com>
2023-02-11 05:07:53 -0700

    support ARNG and XDRNG

--- numba_pokemon_prngs/lcrng/__init__.py
+++ numba_pokemon_prngs/lcrng/__init__.py
@@ -49,4 +49,46 @@ class PokeRNGRDiv(LCRNG32):
     """Reversed Standard Pokemon LCRNG with reciprocal division random distribution"""
 
 
+@numba.experimental.jitclass()
+@lcrng32_init(
+    add=0x1,
+    mult=0x6C078965,
+    distribution=LCRNG32RandomDistribution.MODULO,
+)
+class ARNG(LCRNG32):
+    """Alternative Pokemon LCRNG with modulo random distribution"""
+
+
+@numba.experimental.jitclass()
+@lcrng32_init(
+    add=0x1,
+    mult=0x6C078965,
+    distribution=LCRNG32RandomDistribution.MODULO,
+    reverse=True,
+)
+class ARNGR(LCRNG32):
+    """Reversed Alternative Pokemon LCRNG with modulo random distribution"""
+
+
+@numba.experimental.jitclass()
+@lcrng32_init(
+    add=0x269EC3,
+    mult=0x343FD,
+    distribution=LCRNG32RandomDistribution.MODULO,
+)
+class XDRNG(LCRNG32):
+    """Pokemon Colosseum/XD/Channel LCRNG with modulo random distribution"""
+
+
+@numba.experimental.jitclass()
+@lcrng32_init(
+    add=0x269EC3,
+    mult=0x343FD,
+    distribution=LCRNG32RandomDistribution.MODULO,
+    reverse=True,
+)
+class XDRNGR(LCRNG32):
+    """Reversed Pokemon Colosseum/XD/Channel LCRNG with modulo random distribution"""
+
+
 # pylint: enable=abstract-method
--- tests/test_lcrng.py
+++ tests/test_lcrng.py
@@ -1,13 +1,27 @@
 """Tests for LCRNG classes"""
-from numba_pokemon_prngs.lcrng import PokeRNGDiv, PokeRNGMod, PokeRNGRDiv, PokeRNGRMod
+from numba_pokemon_prngs.lcrng import (
+    PokeRNGDiv,
+    PokeRNGMod,
+    PokeRNGRDiv,
+    PokeRNGRMod,
+    ARNG,
+    ARNGR,
+    XDRNG,
+    XDRNGR,
+)
 
 
 def test_lcrng_next():
     """Test LCRNG next() calls for full seed"""
     test_pokerng_div = PokeRNGDiv(0x12345678)
     test_pokerng_mod = PokeRNGMod(0x12345678)
+    test_arng = ARNG(0x12345678)
+    test_xdrng = XDRNG(0x12345678)
+
     test_pokerngr_div = PokeRNGRDiv(0x12345678)
     test_pokerngr_mod = PokeRNGRMod(0x12345678)
+    test_arngr = ARNGR(0x12345678)
+    test_xdrngr = XDRNGR(0x12345678)
     assert tuple(test_pokerng_div.next() for _ in range(5)) == (
         192004491,
         2229936802,
@@ -22,6 +36,21 @@ def test_lcrng_next():
         4108329948,
         646229279,
     )
+    assert tuple(test_arng.next() for _ in range(5)) == (
+        775181657,
+        499207454,
+        4082793175,
+        2119206356,
+        103760037,
+    )
+    assert tuple(test_xdrng.next() for _ in range(5)) == (
+        3018423131,
+        1530878130,
+        3423460525,
+        3768712380,
+        1536596111,
+    )
+
     assert tuple(test_pokerngr_div.next() for _ in range(5)) == (
         1358145273,
         3431847134,
@@ -36,14 +65,33 @@ def test_lcrng_next():
         3556988756,
         4100256197,
     )
+    assert tuple(test_arngr.next() for _ in range(5)) == (
+        2408337579,
+        2286114914,
+        3692372301,
+        373632348,
+        1342820287,
+    )
+    assert tuple(test_xdrngr.next() for _ in range(5)) == (
+        3745948697,
+        399063950,
+        1307699815,
+        631669108,
+        1486693829,
+    )
 
 
 def test_lcrng_next_u16():
     """Test LCRNG next_u16() calls for 16-bit rand"""
     test_pokerng_div = PokeRNGDiv(0x12345678)
     test_pokerng_mod = PokeRNGMod(0x12345678)
+    test_arng = ARNG(0x12345678)
+    test_xdrng = XDRNG(0x12345678)
+
     test_pokerngr_div = PokeRNGRDiv(0x12345678)
     test_pokerngr_mod = PokeRNGRMod(0x12345678)
+    test_arngr = ARNGR(0x12345678)
+    test_xdrngr = XDRNGR(0x12345678)
     assert tuple(test_pokerng_div.next_u16() for _ in range(5)) == (
         2929,
         34026,
@@ -58,6 +106,21 @@ def test_lcrng_next_u16():
         62688,
         9860,
     )
+    assert tuple(test_arng.next_u16() for _ in range(5)) == (
+        11828,
+        7617,
+        62298,
+        32336,
+        1583,
+    )
+    assert tuple(test_xdrng.next_u16() for _ in range(5)) == (
+        46057,
+        23359,
+        52237,
+        57505,
+        23446,
+    )
+
     assert tuple(test_pokerngr_div.next_u16() for _ in range(5)) == (
         20723,
         52365,
@@ -72,14 +135,33 @@ def test_lcrng_next_u16():
         54275,
         62564,
     )
+    assert tuple(test_arngr.next_u16() for _ in range(5)) == (
+        36748,
+        34883,
+        56341,
+        5701,
+        20489,
+    )
+    assert tuple(test_xdrngr.next_u16() for _ in range(5)) == (
+        57158,
+        6089,
+        19953,
+        9638,
+        22685,
+    )
 
 
 def test_lcrng_next_rand():
     """Test LCRNG next_rand() calls for constrained rand"""
     test_pokerng_div = PokeRNGDiv(0x12345678)
     test_pokerng_mod = PokeRNGMod(0x12345678)
+    test_arng = ARNG(0x12345678)
+    test_xdrng = XDRNG(0x12345678)
+
     test_pokerngr_div = PokeRNGRDiv(0x12345678)
     test_pokerngr_mod = PokeRNGRMod(0x12345678)
+    test_arngr = ARNGR(0x12345678)
+    test_xdrngr = XDRNGR(0x12345678)
     assert tuple(
         tuple(test_pokerng_div.next_rand(maximum) for _ in range(5))
         for maximum in (2, 5, 25, 100, 256)
@@ -101,6 +183,27 @@ def test_lcrng_next_rand():
         (76, 93, 38, 12, 132),
     )
     assert tuple(
+        tuple(test_arng.next_rand(maximum) for _ in range(5))
+        for maximum in (2, 5, 25, 100, 256)
+    ) == (
+        (0, 1, 0, 0, 1),
+        (2, 4, 3, 4, 1),
+        (1, 13, 11, 3, 20),
+        (15, 33, 57, 36, 87),
+        (105, 49, 75, 45, 63),
+    )
+    assert tuple(
+        tuple(test_xdrng.next_rand(maximum) for _ in range(5))
+        for maximum in (2, 5, 25, 100, 256)
+    ) == (
+        (1, 1, 1, 1, 0),
+        (2, 3, 3, 3, 2),
+        (3, 17, 4, 0, 12),
+        (35, 44, 94, 7, 23),
+        (8, 0, 173, 89, 232),
+    )
+
+    assert tuple(
         tuple(test_pokerngr_div.next_rand(maximum) for _ in range(5))
         for maximum in (2, 5, 25, 100, 256)
     ) == (
@@ -119,4 +222,24 @@ def test_lcrng_next_rand():
         (19, 4, 9, 18, 2),
         (56, 39, 23, 86, 53),
         (51, 229, 5, 237, 186),
+    )
+    assert tuple(
+        tuple(test_arngr.next_rand(maximum) for _ in range(5))
+        for maximum in (2, 5, 25, 100, 256)
+    ) == (
+        (0, 1, 1, 1, 1),
+        (4, 1, 1, 0, 0),
+        (6, 22, 15, 11, 1),
+        (18, 95, 43, 14, 43),
+        (65, 82, 251, 148, 64),
+    )
+    assert tuple(
+        tuple(test_xdrngr.next_rand(maximum) for _ in range(5))
+        for maximum in (2, 5, 25, 100, 256)
+    ) == (
+        (0, 1, 1, 0, 1),
+        (3, 4, 4, 0, 1),
+        (19, 3, 21, 8, 23),
+        (14, 84, 97, 77, 67),
+        (253, 125, 122, 133, 99),
     )
