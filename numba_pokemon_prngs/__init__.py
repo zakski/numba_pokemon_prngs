fd4a317f8ccd988e696c0edcc4d4d7d245391780
lincoln-lm <73306575+Lincoln-LM@users.noreply.github.com>
2023-02-15 21:29:28 -0700

    import RNGList into main module

--- numba_pokemon_prngs/__init__.py
+++ numba_pokemon_prngs/__init__.py
@@ -1,6 +1,7 @@
 """Package for numba @jitclass implementations
 of the Pseudo-Random Number Generators used in the Pokemon series"""
 import numpy as np
+from .rng_list import build_rnglist, RNGList
 
 np.seterr(over="ignore")
 del np
