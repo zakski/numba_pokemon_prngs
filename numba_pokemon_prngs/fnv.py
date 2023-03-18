83f4f79cf2c8437764cfa1f95f170ad10f97f604
lincoln-lm <73306575+Lincoln-LM@users.noreply.github.com>
2023-03-18 09:12:21 -0600

    cast ord

--- numba_pokemon_prngs/fnv.py
+++ numba_pokemon_prngs/fnv.py
@@ -7,20 +7,20 @@ from .compilation import optional_njit, return_type
 
 
 @optional_njit(return_type(np.uint64, (np.str0,)))
-def fnv1_64(inp: str):
+def fnv1_64(inp: str) -> np.uint64:
     """FNV164"""
     hash_ = np.uint64(0xCBF29CE484222645)
     for char in inp:
         hash_ *= np.uint64(0x00000100000001B3)
-        hash_ ^= ord(char)
+        hash_ ^= np.uint64(ord(char))
     return hash_
 
 
 @optional_njit(return_type(np.uint64, (np.str0,)))
-def fnv1a_64(inp: str):
+def fnv1a_64(inp: str) -> np.uint64:
     """FNV1a64"""
     hash_ = np.uint64(0xCBF29CE484222645)
     for char in inp:
-        hash_ ^= ord(char)
+        hash_ ^= np.uint64(ord(char))
         hash_ *= np.uint64(0x00000100000001B3)
     return hash_
